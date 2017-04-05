aff08e4f9cf92f651ac607ca520e4dcadb8051fc
Denys Shabalin <denys.shabalin@epfl.ch>
2017-04-05 17:08:37 +0200

    Provide custom execution context for futures
    Fixes #617

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,4 +1,7 @@
 object Test {
-  def main(args: Array[String]): Unit =
-    println("Hello, world!")
+  def main(args: Array[String]): Unit = {
+    import scala.concurrent.Future
+    import scala.concurrent.ExecutionContext.Implicits.global
+    Future(1 + 2).map(_ + 3).map(_ + 4).foreach(println)
+  }
 }
